e84d4071e8d991bbb581d5be348bab7f422b023d
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-01-22 13:40:15 +0000

    añadiendo el mailer para la confirmación del correo

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template "users/new"
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
   end
 
   test "valid signup information" do
@@ -23,7 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template "users/show"
-    assert is_logged_in?
+    # assert_template 'users/show'
+    # assert is_logged_in?
   end
 end
